be30db50e4e4920714c71642816b753919688642
richard <richard.viquez@softland.cr>
2020-10-10 20:57:19 -0600

    2 decimales

--- index.js
+++ index.js
@@ -7,15 +7,62 @@ const app=new Vue({
         haberes:[],
         haber:'',
         debe:'',
-        myDate: new Date()
+        myDate: new Date(),
+        totaldebito:0,
+        totalcredito:0
     },
     methods:{
         agregardebe (){
             
-            this.debes.push(`${this.debe}`)
+            if (this.validarNumero(this.debe)) {
+                let ingreso=parseFloat(this.debe)
+
+                let total=parseFloat(this.totaldebito)
+                this.debes.push(ingreso)
+                let suma=total+ingreso      
+                this.totaldebito=parseFloat(suma).toFixed(2)    
+            } else {
+                this.mostrarMensaje(`El valor ingresado no es numérico`,`error`)
+            }
+           
         },
         agregarhaber(){
-            this.haberes.push(`${this.haber}`)
+            if (this.validarNumero(this.haber)) {
+                let ingreso=parseFloat(this.haber)
+
+                let total=parseFloat(this.totalcredito)
+                this.haberes.push(ingreso)
+                let suma=total+ingreso      
+                this.totalcredito=parseFloat(suma).toFixed(2)  
+            } else {
+                this.mostrarMensaje(`El valor ingresado no es numérico`,`error`)
+            }
+        },
+        mostrarMensaje(msg,tipo){
+
+            Swal.fire({
+                title: 'Atención!',
+                text: msg,
+                icon: tipo,
+                confirmButtonText: 'OK'
+              })
+        },
+        validarNumero(dato){
+
+            try {
+                if ((typeof dato!==null)&&(typeof dato!==undefined)&&
+                (dato!=="")&&!isNaN(dato)) {
+                    let ingreso=parseFloat(dato)
+
+                    if(typeof ingreso === 'number'){
+                        
+                       return `es numero`
+                       
+                    }
+                } 
+            } catch (error) {
+                
+            }
         }
 
     },
